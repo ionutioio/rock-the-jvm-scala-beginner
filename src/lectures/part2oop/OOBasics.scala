a6d033ff6da655637cb1d17b4f2849f2c7f32fd0
iiortoma <ionut.iortoman@capgemini.com>
2020-02-18 17:26:48 +0100

    mlkjmlj

--- src/lectures/part2oop/OOBasics.scala
+++ src/lectures/part2oop/OOBasics.scala
@@ -12,8 +12,22 @@ class Person(name: String, val age: Int) {
 
 }
 
-class Writer(firstName: String, lastName: String, yob: Int){
+class Writer(firstName: String, lastName: String, val yob: Int){
   def fullName() = s"@firstName @lastName"
+}
+class Novel(name: String, year: Int, author: Writer){
+  def authorAge(): Int = year - this.author.yob
+
+  def isWrittenBy(author: Writer) : Boolean = (this.author == author)
+  def copy(year:Int): Novel = new Novel(this.name, year, this.author)
+}
+
+class Counter(val value: Int){
+  def currentCount(): Int = value
+  def inc() = new Counter (this.value +1)
+  def dec() = new Counter(this.value-1)
+  def inc(amount: Int) = new Counter(this.value+amount)
+  def dec(amount: Int) = new Counter(this.value-amount)
 }
 
 //class params are NOT fields.
